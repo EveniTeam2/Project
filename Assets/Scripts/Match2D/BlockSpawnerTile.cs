afdb326e9a214b794a6313201d83fd68bd914fa5
Hwan007 <ghksdlco@gmail.com>
2024-07-04 18:10:24 +0900

    Spawner Tile 생성

---
+++ Assets/Scripts/Match2D/BlockSpawnerTile.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Match2D {
+    [CreateAssetMenu(fileName = "BlockSpawnerTile", menuName = "Tile/Block Spawner Tile")]
+    public class BlockSpawnerTile : TileBase {
+        public Sprite EditorPreviewSprite;
+
+        public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData) {
+            tileData.sprite = !Application.isPlaying ? EditorPreviewSprite : null;
+        }
+
+        public override bool StartUp(Vector3Int position, ITilemap tilemap, GameObject go) {
+#if UNITY_EDITOR
+            if (!Application.isPlaying)
+                return false;
+#endif
+            //Board.RegisterSpawner(position);
+            return base.StartUp(position, tilemap, go);
+        }
+    }
+}
