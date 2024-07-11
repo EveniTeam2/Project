893fe432075ecc75c1a85876870ac8e90c6de243
Hwan007 <ghksdlco@gmail.com>
2024-07-12 03:02:53 +0900

    Remove SubState를 주석처리함

--- Assets/Scripts/Unit/Character/StateBuilder.cs
+++ Assets/Scripts/Unit/Character/StateBuilder.cs
@@ -19,48 +19,48 @@ namespace Unit.Character {
             var sm = new StateMachine(target);
 
             foreach (var stateData in data.StateDatas) {
-                if (stateData.States.Count > 0) {
-                    var sub = BuildSubState(sm, stateData);
-                    sm.TryAddState(stateData.StateName, sub);
-                }
-                else {
+                //if (stateData.States.Count > 0) {
+                //    var sub = BuildSubState(sm, stateData);
+                //    sm.TryAddState(stateData.StateName, sub);
+                //}
+                //else {
                     var state = BuildState(sm, stateData);
                     sm.TryAddState(stateData.StateName, state);
-                }
+                //}
             }
             return sm;
         }
 
-        private static SubState BuildSubState(StateMachine sm, StateData data) {
-            Debug.Assert(data.States.Count > 0, "SubState에 최소한 1개의 BaseState가 있어야 합니다.");
+        //private static SubState BuildSubState(StateMachine sm, StateData data) {
+        //    Debug.Assert(data.States.Count > 0, "SubState에 최소한 1개의 BaseState가 있어야 합니다.");
 
-            Action<IState> enter = null;
-            if (data.OnEnter != null)
-                enter = data.OnEnter.OnAct;
-            Action<IState> exit = null;
-            if (data.OnExit != null)
-                exit = data.OnExit.OnAct;
-            Action<IState> update = null;
-            if (data.OnUpdate != null)
-                update = data.OnUpdate.OnAct;
-            Action<IState> fixedupdate = null;
-            if (data.OnFixedUpdate != null)
-                fixedupdate = data.OnFixedUpdate.OnAct;
-            Func<BaseCharacter, bool> condition = null;
-            if (data.Condition != null)
-                condition = data.Condition.CheckCondition;
+        //    Action<IState> enter = null;
+        //    if (data.OnEnter != null)
+        //        enter = data.OnEnter.OnAct;
+        //    Action<IState> exit = null;
+        //    if (data.OnExit != null)
+        //        exit = data.OnExit.OnAct;
+        //    Action<IState> update = null;
+        //    if (data.OnUpdate != null)
+        //        update = data.OnUpdate.OnAct;
+        //    Action<IState> fixedupdate = null;
+        //    if (data.OnFixedUpdate != null)
+        //        fixedupdate = data.OnFixedUpdate.OnAct;
+        //    Func<BaseCharacter, bool> condition = null;
+        //    if (data.Condition != null)
+        //        condition = data.Condition.CheckCondition;
 
-            int hash = 0;
-            if (data.AnimParameter.Length > 0) {
-                hash = Animator.StringToHash(data.AnimParameter);
-            }
-            var sub = new SubState(sm, BuildState(sm, data.States[0]), data.StateName, hash, enter, exit, update, fixedupdate, condition);
+        //    int hash = 0;
+        //    if (data.AnimParameter.Length > 0) {
+        //        hash = Animator.StringToHash(data.AnimParameter);
+        //    }
+        //    var sub = new SubState(sm, BuildState(sm, data.States[0]), data.StateName, hash, enter, exit, update, fixedupdate, condition);
 
-            for (int i = 1; i < data.States.Count; ++i) {
-                sub.TryAddState(data.States[i].StateName, BuildState(sm, data.States[i]));
-            }
-            return sub;
-        }
+        //    for (int i = 1; i < data.States.Count; ++i) {
+        //        sub.TryAddState(data.States[i].StateName, BuildState(sm, data.States[i]));
+        //    }
+        //    return sub;
+        //}
 
         private static BaseState BuildState(StateMachine sm, StateData data) {
             Action<IState> enter = null;
