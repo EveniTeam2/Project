08a5899d99bab5142ac5b7f946e55646a4a7c71d
Hwan007 <ghksdlco@gmail.com>
2024-07-12 00:31:37 +0900

    Modify 유니티 컴포넌트는 가져다 넣게 만듬.

--- Assets/Scripts/Unit/Character/PlayerCharacter.cs
+++ Assets/Scripts/Unit/Character/PlayerCharacter.cs
@@ -12,21 +12,24 @@ namespace Unit.Character {
         public int Attack => _stats.Current.Attack;
         [SerializeField] CharacterStateData characterStateData;
         UserInput _input;
+        public event Action<IRunnable> OnRun;
+        public LinkedList<int> spdModifier = new LinkedList<int>();
+        public bool IsRun { get; protected set; }
+        public override int Health { get => _stats.Current.Health; protected set => _stats.Current.Health = value; }
+        public override Animator Animator => animator;
+        [SerializeField] Animator animator;
         public virtual void Input(NewBlock block, int count) {
             _input.Input(block, count);
             // Idle run walk hit
             // 체력 회복 블록, 스킬 1번 실행 블론, 스킬 2번 ~ 4번
 
         }
-
         public virtual float GetCurrentPosition() {
             return 0;
         }
-
         public virtual void SetRun(bool isRun) {
             IsRun = isRun;
         }
-
         protected virtual void RecalculateSpeed() {
             if (IsRun) {
                 _stats.Current.Speed = _stats.Origin.Speed;
@@ -35,22 +38,11 @@ namespace Unit.Character {
                 }
             }
         }
-
         public void Initialize(PlayerStat stat, IShowable background) {
             _stats = new InstanceStat<PlayerStat>(stat);
             OnRun += background.Move;
             StateBuilder.BuildState(HFSM, characterStateData);
-            animator = transform.GetChild(0).GetComponent<Animator>();
         }
-
-        public event Action<IRunnable> OnRun;
-        public LinkedList<int> spdModifier = new LinkedList<int>();
-        public bool IsRun { get; protected set; }
-        public override int Health { get => _stats.Current.Health; protected set => _stats.Current.Health = value; }
-
-        public override Animator Animator => animator;
-        [SerializeField] Animator animator;
-
         protected static int spdUnit = 1000;
         public int ModifySpeed(int spd, float duration) {
             if (duration > 0) {
@@ -74,7 +66,6 @@ namespace Unit.Character {
                 OnRun?.Invoke(this);
             }
         }
-
         public override void SetHealth(int health) {
             _stats.Origin.Health = health;
             _stats.Current.Health = health;
