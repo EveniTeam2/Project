7ab290943936870410fb2b1269d3b1cf5e9d9e0d
Hwan007 <ghksdlco@gmail.com>
2024-07-13 20:48:57 +0900

    Modify Change Unity menu name

--- Assets/Scripts/Unit/Stages/Creatures/FSM/ActOnInput/ChangeState.cs
+++ Assets/Scripts/Unit/Stages/Creatures/FSM/ActOnInput/ChangeState.cs
@@ -5,7 +5,7 @@ namespace Unit.Stages.Creatures {
     /// <summary>
     /// 입력에 따라 상태를 바꾸는 행동입니다.
     /// </summary>
-    [CreateAssetMenu(fileName = nameof(ChangeState), menuName = "SO/" + nameof(ChangeState))]
+    [CreateAssetMenu(fileName = nameof(ChangeState), menuName = "Input/" + nameof(ChangeState))]
     public class ChangeState : ActCharacter {
         public override void Act(ActOnInput inputData, Character character, int count) {
             character.HFSM.TryChangeState(inputData.StateName);
--- Assets/Scripts/Unit/Stages/Creatures/FSM/ActOnInput/CompositeActCharacter.cs
+++ Assets/Scripts/Unit/Stages/Creatures/FSM/ActOnInput/CompositeActCharacter.cs
@@ -5,7 +5,7 @@ namespace Unit.Stages.Creatures {
     /// <summary>
     /// 여러 act를 묶어서 순차적으로 실행하는 클래스입니다.
     /// </summary>
-    [CreateAssetMenu(fileName = nameof(CompositeActCharacter), menuName = "SO/" + nameof(CompositeActCharacter))]
+    [CreateAssetMenu(fileName = nameof(CompositeActCharacter), menuName = "Input/" + nameof(CompositeActCharacter))]
     public class CompositeActCharacter : ActCharacter {
         [SerializeField] private ActCharacter[] compositeActs;
         public override void Act(ActOnInput inputData, Character character, int count) {
