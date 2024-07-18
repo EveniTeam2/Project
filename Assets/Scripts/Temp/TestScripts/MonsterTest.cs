71c64a0d322618c917669bec72c7f70baf757525
Hwan007 <ghksdlco@gmail.com>
2024-07-19 03:03:25 +0900

    Merge fix

--- Assets/Scripts/Temp/TestScripts/MonsterTest.cs
+++ Assets/Scripts/Temp/TestScripts/MonsterTest.cs
@@ -16,23 +16,13 @@ namespace Temp {
         public KeyCode key;
         public CommandPacket packet;
     }
-    public class MonsterTest : MonoBehaviour, IStage {
-        [SerializeField] StageScore stageScore;
-        [SerializeField] Unit.GameScene.Stages.Creatures.Units.Characters.Character player;
+    public class MonsterTest : StageManager, IStage {
         [SerializeField] SceneExtraSetting extraSetting;
         [SerializeField] SceneDefaultSetting defaultSetting;
         [SerializeField] KeyCode _restart;
         [SerializeField] TestCommand[] testCommand;
-
-        private float _startTime;
-        private MonsterSpawnManager _spawnManager;
-        private CameraController cam;
-
-        public float PlayTime => Time.time - _startTime;
-        public float Distance => player.transform.position.x - defaultSetting.playerSpawnPosition.x;
-        public Unit.GameScene.Stages.Creatures.Units.Characters.Character Character => player;
-        public LinkedList<Monster> Monsters => _spawnManager.Monsters;
-
+        [SerializeField] Camera _camera;
+        CameraController cam;
         void Start() {
             onInput = InitGame;
         }
@@ -54,44 +44,20 @@ namespace Temp {
         void StopGame() {
             onInput = InitGame;
 
-            Destroy(player);
-            _spawnManager.Clear();
+            Destroy(_character);
+            _monsterManager.Clear();
             cam.Initialize(null);
         }
 
         void InitGame() {
             onInput = StopGame;
             var _characterSetting = new CharacterSetting(extraSetting.characterDefaultSetting, extraSetting.characterExtraSetting);
-
-            InitializeCharacter(_characterSetting, defaultSetting.playerSpawnPosition);
-            InitializeMonster(extraSetting, defaultSetting.playerSpawnPosition);
-            InitializeCamera(defaultSetting.mainCamera);
-
-            _spawnManager.Start();
+            Initialize(_characterSetting, defaultSetting.playerSpawnPosition, extraSetting, defaultSetting, _camera);
         }
 
-        private void InitializeCharacter(CharacterSetting characterSetting, Vector3 playerSpawnPosition) {
-            var character = Instantiate(characterSetting.Prefab, playerSpawnPosition, Quaternion.identity);
-
-            _startTime = Time.time;
-
-            if (character.TryGetComponent(out player)) {
-                player.Initialize(characterSetting, playerSpawnPosition.y);
-            }
-            player.GetServiceProvider().RegistEvent(ECharacterEventType.Death, GetPlayerIsDead);
-        }
-
-        private void GetPlayerIsDead() {
-            Debug.Log("Player Dead.");
-        }
-
-        private void InitializeMonster(SceneExtraSetting extraSetting, Vector3 playerSpawnPosition) {
-            _spawnManager = new MonsterSpawnManager(player.transform, extraSetting.monsterSpawnData, playerSpawnPosition.y, stageScore);
-        }
-
-        private void InitializeCamera(Camera cam) {
+        protected override void InitializeCamera(Camera cam) {
             this.cam = cam.GetComponent<CameraController>();
-            this.cam.Initialize(player.transform);
+            this.cam.Initialize(_character.transform);
         }
     }
 }
